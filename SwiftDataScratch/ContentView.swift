4c12f0ca50be2370656c339a73c10254dc73c322
Nitish M <mnitish1705@gmail.com>
2025-11-16 10:34:04 +0530

    Initial Commit

---
+++ SwiftDataScratch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataScratch
+//
+//  Created by Nitish M on 16/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataScratch/SwiftDataScratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataScratchApp.swift
+//  SwiftDataScratch
+//
+//  Created by Nitish M on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataScratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
